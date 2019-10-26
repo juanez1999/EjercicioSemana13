2ab48bc3fab5b2a855e9021f7fbcc01aec595874
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2019-10-25 21:04:06 -0500

    Se agregan más rutas api, solo falta los de pasado y futuro

--- routes.js
+++ routes.js
@@ -51,45 +51,118 @@ function createRoutes (app, db) {
             response.send(result);
         });
     });
-    
 
-    
-    app.post('/api/carItems',(request,response) => {
-        console.log(request.body);
-
-        const products = db.collection('products');
-        products.insert(request.body);
-
-        response.send({
-            message: 'ok'
-        });
-    });
-
-    app.delete('/api/carItems',(request,response) =>{
-        var index = request.body.indexToDelete;
-        console.log(index);
-        const products = db.collection('products');
-        products.deleteOne({
-            _id: new ObjectID(index),
-        },function(err,result){
-            console.log(err,result);
-        });
-        response.send({
-            message: 'deleted',
-        });
-    });
-    
-    app.put('/api/carItems',(request,response) =>{
-        var elementToEdit = carItems[0];
-        elementToEdit.name= 'Nuevo nombre';
-        elementToEdit.wheels= '100';
-        elementToEdit.color= 'Green';
-        response.send({
-            message: 'edited',
-        });
+    app.get('/api/heavier', (request, response) => {
+        const products = db.collection('tiposDinosaurios');
         
+        //buscamos todos los productos
+        products.find({ weight: {$gt:2.5}})
+        //transformamos el cursor a una arreglo
+        .toArray((err,result) => {
+            //aseguramos de que no hay error
+            assert.equal(null,err);
+            
+            response.send(result);
+        });
     });
-    
+
+    app.get('/api/tallerAndHeavier', (request, response) => {
+        const products = db.collection('tiposDinosaurios');
+        
+        //buscamos todos los productos
+        products.find({ height: {$gt:26}, weight: {$gt:2.9}})
+        //transformamos el cursor a una arreglo
+        .toArray((err,result) => {
+            //aseguramos de que no hay error
+            assert.equal(null,err);
+            
+            response.send(result);
+        });
+    });
+
+    app.get('/api/tallest', (request, response) => {
+        const products = db.collection('tiposDinosaurios');
+        
+        //buscamos todos los productos
+        products.find().sort({height:-1}).limit(1)
+        //transformamos el cursor a una arreglo
+        .toArray((err,result) => {
+            //aseguramos de que no hay error
+            assert.equal(null,err);
+            
+            response.send(result);
+        });
+    });
+
+    app.get('/api/widest', (request, response) => {
+        const products = db.collection('tiposDinosaurios');
+        
+        //buscamos todos los productos
+        products.find().sort({width:-1}).limit(1)
+        //transformamos el cursor a una arreglo
+        .toArray((err,result) => {
+            //aseguramos de que no hay error
+            assert.equal(null,err);
+            
+            response.send(result);
+        });
+    });
+
+    app.get('/api/heaviest', (request, response) => {
+        const products = db.collection('tiposDinosaurios');
+        
+        //buscamos todos los productos
+        products.find().sort({weight:-1}).limit(1)
+        //transformamos el cursor a una arreglo
+        .toArray((err,result) => {
+            //aseguramos de que no hay error
+            assert.equal(null,err);
+            
+            response.send(result);
+        });
+    });
+
+    app.get('/api/colombia', (request, response) => {
+        const products = db.collection('tiposDinosaurios');
+        
+        //buscamos todos los productos
+        products.find({ countries: {$eq: "Colombia"}})
+        //transformamos el cursor a una arreglo
+        .toArray((err,result) => {
+            //aseguramos de que no hay error
+            assert.equal(null,err);
+            
+            response.send(result);
+        });
+    });
+
+    app.get('/api/noFranceNoItaly', (request, response) => {
+        const products = db.collection('tiposDinosaurios');
+        
+        //buscamos todos los productos
+        products.find({ countries: {$ne: "France", $ne:"Italy"}, diet: {$eq:"carnivorous"}})
+        //transformamos el cursor a una arreglo
+        .toArray((err,result) => {
+            //aseguramos de que no hay error
+            assert.equal(null,err);
+            
+            response.send(result);
+        });
+    });
+
+    app.get('/api/russia', (request, response) => {
+        const products = db.collection('tiposDinosaurios');
+        
+        //buscamos todos los productos
+        products.find({ countries: {$eq: "Russia"}, diet: {$ne:"carnivorous"}}).sort({weight:-1})
+        //transformamos el cursor a una arreglo
+        .toArray((err,result) => {
+            //aseguramos de que no hay error
+            assert.equal(null,err);
+            
+            response.send(result);
+        });
+    });
     
 }
 
